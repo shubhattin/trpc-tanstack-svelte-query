81d42471e8e7c6321a407f28be3429524a31a756
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-06-06 09:19:13 +0530

    form

--- test-app/src/routes/+page.svelte
+++ test-app/src/routes/+page.svelte
@@ -2,8 +2,8 @@
 	import { createQuery } from "@tanstack/svelte-query";
 	import { client } from "$lib/trpc/client";
 
-	const a = 2;
-	const b = 3;
+	let a = $state(2);
+	let b = $state(3);
 
 	const addQuery = createQuery(() => ({
 		queryKey: ["add", a, b],
@@ -13,6 +13,17 @@
 
 <h1>tRPC + TanStack Query</h1>
 
+<form class="form" onsubmit={(e) => e.preventDefault()}>
+	<label>
+		a
+		<input type="number" bind:value={a} />
+	</label>
+	<label>
+		b
+		<input type="number" bind:value={b} />
+	</label>
+</form>
+
 {#if addQuery.isPending}
 	<p>Loading...</p>
 {:else if addQuery.isError}
@@ -20,3 +31,22 @@
 {:else}
 	<p>{a} + {b} = {addQuery.data}</p>
 {/if}
+
+<style>
+	.form {
+		display: flex;
+		gap: 1rem;
+		margin-bottom: 1rem;
+	}
+
+	label {
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+	}
+
+	input {
+		width: 5rem;
+		padding: 0.25rem 0.5rem;
+	}
+</style>
